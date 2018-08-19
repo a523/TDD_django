b26df81599566ca7aca8c686e2103230c983891d
a523 <chjgreen@qq.com>
2018-08-19 18:15:56 +0800

    Use Bootstrap to improve layout

--- functional_test/tests.py
+++ functional_test/tests.py
@@ -1,9 +1,9 @@
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Chrome()
         self.browser.implicitly_wait(3)
@@ -20,16 +20,6 @@ class NewVisitorTest(LiveServerTestCase):
         # 阿洁听说有一个很酷的在线待办事项应用
         # 她去看了这个应用的首页
         self.browser.get(self.live_server_url)
-        self.browser.set_window_size(1024, 768)
-
-        # 她看到输入框完美地居中显示
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width']/2, 512, delta=5)
-
-        # 她新建一个清单，看到输入框认完美地居中显示
-        inputbox.send_keys('testing\n')
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width']/2, 512, delta=5)
 
         # 她注意到网页的标题和头部包含"To-Do"这个词
         self.assertIn('To-Do', self.browser.title)
@@ -94,3 +84,17 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertIn('Buy milk', page_text)
 
         # 两人都很满意，去睡觉了
+
+    def test_layout_and_stying(self):
+        # 阿洁访问首页
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # 她看到输入框完美地居中显示
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width']/2, 512, delta=5)
+
+        # 她新建一个清单，看到输入框认完美地居中显示
+        inputbox.send_keys('testing\n')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width']/2, 512, delta=5)
